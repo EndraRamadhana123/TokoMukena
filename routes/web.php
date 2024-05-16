71a4821283978d284b585c36f636ad9b75eb3927
Endra Ramadhana <endraramadhana20@gmail.com>
2024-05-17 02:11:21 +0700

    Initial Commit

---
+++ routes/web.php
@@ -0,0 +1,112 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PerusahaanController;
+use App\Http\Controllers\CoaController;
+use App\Http\Controllers\ContohformController;
+use App\Http\Controllers\PenjualanController;
+use App\Http\Controllers\PembayaranController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JurnalController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    // return view('welcome');
+    return redirect('login');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+// dashboardbootstrap
+Route::get('/dashboardbootstrap', function () {
+    return view('dashboardbootstrap');
+})->middleware(['auth'])->name('dashboardbootstrap');
+
+// route untuk validasi login
+Route::post('/validasi_login', [App\Http\Controllers\LoginController::class, 'show']);
+
+// route selamat
+Route::get('/selamat', function () {
+    return view('selamat',['nama'=>'Hendro Jokondo-kondo']);
+});
+
+// route contoh1
+Route::get('/contoh1', [App\Http\Controllers\Contoh1Controller::class, 'show']);
+Route::get('teshelpercustom', [App\Http\Controllers\Contoh1Controller::class, 'teshelpercustom']);
+Route::get('contohdom', [App\Http\Controllers\Contoh1Controller::class,'contohdom']);
+Route::get('contohajax', [App\Http\Controllers\Contoh1Controller::class,'contohajax']);
+Route::get('contoh1/pdf', [App\Http\Controllers\Contoh1Controller::class,'tescetakpdf']);
+Route::get('contoh1/teshelperlaravel', [App\Http\Controllers\Contoh1Controller::class,'teshelperlaravel']);
+// route contoh2
+Route::get('/contoh2', [App\Http\Controllers\Contoh2Controller::class, 'show']);
+// route coa
+// Route::get('/coa', [App\Http\Controllers\CoaController::class, 'index']);
+// untuk master data coa
+// jika ingin menambahkan routes baru selain default resource di tambah di awal
+// sebelum route resource
+Route::get('coa/tabel', [App\Http\Controllers\CoaController::class,'tabel'])->middleware(['auth']);
+Route::get('coa/fetchcoa', [App\Http\Controllers\CoaController::class,'fetchcoa'])->middleware(['auth']);
+Route::get('coa/fetchAll', [App\Http\Controllers\CoaController::class,'fetchAll'])->middleware(['auth']);
+Route::get('coa/edit/{id}', [App\Http\Controllers\CoaController::class,'edit'])->middleware(['auth']);
+Route::get('coa/destroy/{id}', [App\Http\Controllers\CoaController::class,'destroy'])->middleware(['auth']);
+Route::resource('coa', CoaController::class)->middleware(['auth']);
+
+// contoh form
+Route::get('contohform/fetchAll', [App\Http\Controllers\ContohformController::class,'fetchAll'])->middleware(['auth']);
+Route::get('contohform/fetchcontohform', [App\Http\Controllers\ContohformController::class,'fetchcontohform'])->middleware(['auth']);
+Route::get('contohform/edit/{id}', [App\Http\Controllers\ContohformController::class,'edit'])->middleware(['auth']);
+Route::get('contohform/destroy/{id}', [App\Http\Controllers\ContohformController::class,'destroy'])->middleware(['auth']);
+Route::resource('contohform', ContohformController::class)->middleware(['auth']);
+
+// route ke master data perusahaan
+Route::resource('/perusahaan', PerusahaanController::class)->middleware(['auth']);
+Route::get('/perusahaan/destroy/{id}', [App\Http\Controllers\PerusahaanController::class,'destroy'])->middleware(['auth']);
+
+// untuk transaksi penjualan
+Route::get('penjualan/barang/{id}', [App\Http\Controllers\PenjualanController::class,'getDataBarang'])->middleware(['auth']);
+Route::get('penjualan/keranjang', [App\Http\Controllers\PenjualanController::class,'keranjang'])->middleware(['auth']);
+Route::get('penjualan/destroypenjualandetail/{id}', [App\Http\Controllers\PenjualanController::class,'destroypenjualandetail'])->middleware(['auth']);
+Route::get('penjualan/barang', [App\Http\Controllers\PenjualanController::class,'getDataBarangAll'])->middleware(['auth']);
+Route::get('penjualan/jmlbarang', [App\Http\Controllers\PenjualanController::class,'getJumlahBarang'])->middleware(['auth']);
+Route::get('penjualan/keranjangjson', [App\Http\Controllers\PenjualanController::class,'keranjangjson'])->middleware(['auth']);
+Route::get('penjualan/checkout', [App\Http\Controllers\PenjualanController::class,'checkout'])->middleware(['auth']);
+Route::get('penjualan/invoice', [App\Http\Controllers\PenjualanController::class,'invoice'])->middleware(['auth']);
+Route::get('penjualan/jmlinvoice', [App\Http\Controllers\PenjualanController::class,'getInvoice'])->middleware(['auth']);
+Route::get('penjualan/status', [App\Http\Controllers\PenjualanController::class,'viewstatus'])->middleware(['auth']);
+Route::resource('penjualan', PenjualanController::class)->middleware(['auth']);
+
+// transaksi pembayaran viewkeranjang
+Route::get('pembayaran/viewkeranjang', [App\Http\Controllers\PembayaranController::class,'viewkeranjang'])->middleware(['auth']);
+Route::get('pembayaran/viewstatus', [App\Http\Controllers\PembayaranController::class,'viewstatus'])->middleware(['auth']); 
+Route::get('pembayaran/viewapprovalstatus', [App\Http\Controllers\PembayaranController::class,'viewapprovalstatus'])->middleware(['auth']);
+Route::get('pembayaran/approve/{no_transaksi}', [App\Http\Controllers\PembayaranController::class,'approve'])->middleware(['auth']);
+Route::get('pembayaran/unapprove/{no_transaksi}', [App\Http\Controllers\PembayaranController::class,'unapprove'])->middleware(['auth']);
+Route::get('pembayaran/viewstatusPG', [App\Http\Controllers\PembayaranController::class,'viewstatusPG'])->middleware(['auth']);
+Route::get('pembayaran/checkout', [PembayaranController::class, 'checkout'])->name('pembayaran.checkout');
+Route::resource('pembayaran', PembayaranController::class)->middleware(['auth']);
+
+// grafik
+//Route::get('grafik/viewPenjualanBlnBerjalan', [App\Http\Controllers\GrafikController::class,'viewPenjualanBlnBerjalan'])->middleware(['auth']);
+//Route::get('grafik/viewJmlPenjualan', [App\Http\Controllers\GrafikController::class,'viewJmlPenjualan'])->middleware(['auth']);
+//Route::get('grafik/viewJmlBarangTerjual', [App\Http\Controllers\GrafikController::class,'viewJmlBarangTerjual'])->middleware(['auth']);
+//Route::get('grafik/viewPenjualanSelectOption/{tahun}', [App\Http\Controllers\GrafikController::class,'viewPenjualanSelectOption'])->middleware(['auth']);
+//Route::get('grafik/viewDataPenjualanSelectOption/{tahun}', [App\Http\Controllers\GrafikController::class,'viewDataPenjualanSelectOption'])->middleware(['auth']);
+
+// laporan
+Route::get('jurnal/umum', [App\Http\Controllers\JurnalController::class,'jurnalumum'])->middleware(['auth']);
+Route::get('jurnal/viewdatajurnalumum/{periode}', [App\Http\Controllers\JurnalController::class,'viewdatajurnalumum'])->middleware(['auth']);
+Route::get('jurnal/bukubesar', [App\Http\Controllers\JurnalController::class,'bukubesar'])->middleware(['auth']);
+Route::get('jurnal/viewdatabukubesar/{periode}/{akun}', [App\Http\Controllers\JurnalController::class,'viewdatabukubesar'])->middleware(['auth']);
+
+require __DIR__.'/auth.php';
